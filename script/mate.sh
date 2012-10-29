502240f835ab8120b4eaae180d550d2ff0e4537a
Pietro Di Bello <pietro.dibello@xpeppers.com>
2012-10-29 22:18:46 +0100

    moving .rvmrc to gitignore, adding a rvm template

--- script/mate.sh
+++ script/mate.sh
@@ -1,1 +1,1 @@
-mate $(ls -a | grep -v '\.\.$' | grep -v '\.$' | grep -v '\.git'| grep -v '\.DS_Store'| grep -v '\.tmtags' | grep -v '\tags')
+mate $(ls -a | grep -v '\.\.$' | grep -v '\.$' | grep -v '\.git$'| grep -v '\.DS_Store'| grep -v '\.tmtags' | grep -v '\tags')

5dcba7ceac5d923cf0ca8465b27862ad99a9d340
Pietro Di Bello <pietro.dibello@xpeppers.com>
2012-12-20 00:29:17 +0100

    fixing bug: only the tracking notification where the tracker user is mentioned should be considered

--- lib/patches/trello/member.rb
+++ lib/patches/trello/member.rb
@@ -2,13 +2,17 @@ module Trello
 
   class Member
     def notifications_from(from_date)
-      notifications(limit:1000).select &greater_than_or_equal_to(from_date)
+      notifications(limit:1000).select(&greater_than_or_equal_to(from_date)).select(&tracking_notification?)
     end
     
     private
 
     def greater_than_or_equal_to(from_date)
       lambda { |notification| Chronic.parse(notification.date) >= from_date }
+    end
+
+    def tracking_notification?
+      lambda { |notification| notification.type == "mentionedOnCard" }
     end
     
   end

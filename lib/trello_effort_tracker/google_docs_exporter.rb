4ba8bf5b9a2aaf8b12a0370db0504ccb112e8ba2
Pietro Di Bello <pietro.dibello@xpeppers.com>
2012-12-28 00:16:02 +0100

    adding more fields to the google docs exporter

--- lib/trello_effort_tracker/google_docs_exporter.rb
+++ lib/trello_effort_tracker/google_docs_exporter.rb
@@ -10,10 +10,12 @@ class GoogleDocsExporter
 
   def export
     columns = {
-      user_story_name:  1,
-      start_date:       2,
-      total_effort:     3,
-      estimate:         4,
+      user_story_id:        1,
+      user_story_name:      2,
+      start_date:           3,
+      total_effort:         4,
+      last_estimate_error:  5,
+      estimate:             6,
     }
 
     spreadsheet = google_docs_session.spreadsheet_by_title(@spreadsheet)
@@ -24,9 +26,11 @@ class GoogleDocsExporter
     cards = TrackedCard.all.reject(&:no_tracking?).sort_by(&:first_activity_date).reverse
     cards.each do |card|
       print ".".color(:green)
+      backlog[index, columns[:user_story_id]] = card.short_id
       backlog[index, columns[:user_story_name]] = card.name
       backlog[index, columns[:start_date]] = card.working_start_date
       backlog[index, columns[:total_effort]] = card.total_effort
+      backlog[index, columns[:last_estimate_error]] = card.last_estimate_error
       card.estimates.each_with_index do |estimate, i|
         backlog[index, columns[:estimate]+i] = estimate.amount
       end

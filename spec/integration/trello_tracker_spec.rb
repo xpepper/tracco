66189c72547a2a3b3aa4d2a6d683c546e033fec5
Pietro Di Bello <pietro.dibello@xpeppers.com>
2013-01-29 00:28:23 +0100

    fixing unstable spec

--- spec/integration/trello_tracker_spec.rb
+++ spec/integration/trello_tracker_spec.rb
@@ -10,14 +10,15 @@ describe TrelloTracker do
     Trello.logger.level = @original_error_level
   end
 
-  xit "tracks an estimate", :needs_valid_configuration => true do
+  it "tracks an estimate", :needs_valid_configuration => true do
     # auth params for trackinguser_for_test/testinguser!
     trackinguser_auth_params = { "developer_public_key" => "ef7c400e711057d7ba5e00be20139a33",
                                  "access_token_key"     => "9047d8fdbfdc960d41910673e300516cc8630dd4967e9b418fc27e410516362e" }
 
     with_trackinguser("trackinguser_for_test") do
       tracker = TrelloTracker.new(trackinguser_auth_params)
-      tracker.track
+      tracker.track(DateTime.parse("2013-01-28"))
+
 
       tracked_card = TrackedCard.first
       tracked_card.estimates.should have(1).estimate
@@ -33,7 +34,7 @@ describe TrelloTracker do
       ENV["tracker_username"] = tracking_user
       block.call unless block.nil?
     ensure
-      ENV["tracker_username"] = original_tracker unless original_tracker.nil?
+      ENV["tracker_username"] = original_tracker
     end
   end
 end

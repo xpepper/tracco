9ac48475b21fc1cc0df6de2bfb50912ee21375d0
Pietro Di Bello <pietro.dibello@xpeppers.com>
2013-02-21 23:41:52 +0100

    refactoring the estimate tracking parsing process

--- lib/tracco/tracking/estimate_tracking.rb
+++ lib/tracco/tracking/estimate_tracking.rb
@@ -2,11 +2,8 @@ module Tracking
   class EstimateTracking
     include Base
 
-    #TODO: rename to 'amount' ?
-    #TODO: avoid recomputing estimate every time using a lazy instance variable
     def estimate
-      estimate = convert_to_hours(raw_estimate)
-      Estimate.new(amount: estimate, date: date, tracking_notification_id: tracking_notification.id)
+      @estimate ||= Estimate.new(amount: convert_to_hours(raw_estimate), date: date, tracking_notification_id: tracking_notification.id)
     end
 
     def add_to(card)

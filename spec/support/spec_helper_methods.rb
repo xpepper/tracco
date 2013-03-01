2f9626ba8f9219f893fd87597ba868e50dcb4d25
Pietro Di Bello <pietro.dibello@xpeppers.com>
2013-03-01 23:13:18 +0100

    improving spec using a fake tracker username instead of the one defined in the config file

--- spec/support/spec_helper_methods.rb
+++ spec/support/spec_helper_methods.rb
@@ -6,7 +6,7 @@ TIME_MEASUREMENTS = {
 }
 
 def unrecognized_notification
-  create_notification(data: { 'text' => '@trackinguser hi there!' })
+  create_notification(data: { 'text' => '@any_tracker hi there!' })
 end
 
 def notification_with_message(message)
@@ -14,11 +14,11 @@ def notification_with_message(message)
 end
 
 def create_estimate(time_measurement)
-  create_notification(data: { 'text' => "@trackinguser [1.5#{TIME_MEASUREMENTS[time_measurement]}]" })
+  create_notification(data: { 'text' => "@any_tracker [1.5#{TIME_MEASUREMENTS[time_measurement]}]" })
 end
 
 def create_effort(time_measurement)
-  create_notification(data: { 'text' => "@trackinguser +4.5#{TIME_MEASUREMENTS[time_measurement]}]" })
+  create_notification(data: { 'text' => "@any_tracker +4.5#{TIME_MEASUREMENTS[time_measurement]}]" })
 end
 
 def with(notification)
@@ -31,7 +31,7 @@ def with_message(notification_message, &block)
 end
 
 def create_notification(custom_params)
-  params = { data: { 'text' => "@trackinguser +2h" }, date: "2012-10-28T21:06:14.801Z", member_creator: stub(username: "pietrodibello") }
+  params = { data: { 'text' => "@any_tracker +2h" }, date: "2012-10-28T21:06:14.801Z", member_creator: stub(username: "pietrodibello") }
   params.merge!(custom_params)
 
   stub(data: params[:data], date: params[:date], member_creator: params[:member_creator]).as_null_object
--- spec/tracking/effort_tracking_spec.rb
+++ spec/tracking/effort_tracking_spec.rb
@@ -12,6 +12,13 @@ module Tracco
 
         before(:each) do
           Trello::Member.stub(:find).and_return(Member.new(username: "any"))
+
+          @original_tracker = ENV["tracker_username"]
+          ENV["tracker_username"] = "test_tracker"
+        end
+
+        after(:each) do
+          ENV["tracker_username"] = @original_tracker
         end
 
         it "is nil when the notification does not contain an estimate" do
@@ -19,13 +26,13 @@ module Tracco
         end
 
         it "does not parse effort in minutes (e.g. +30m)" do
-          with_message("@trackinguser +30m") { |tracking| tracking.effort.should be_nil }
+          with_message("@test_tracker +30m") { |tracking| tracking.effort.should be_nil }
         end
 
         it "is the hour-based effort when the notification contains an effort in hours" do
           Trello::Member.should_receive(:find).with("michelepangrazzi").and_return(michelepangrazzi)
 
-          raw_data = create_notification(data: { 'text' => "@trackinguser +2h" },
+          raw_data = create_notification(data: { 'text' => "@test_tracker +2h" },
                                          date: "2012-10-28T21:06:14.801Z",
                                          member_creator: stub(username: "michelepangrazzi"))
 
@@ -33,28 +40,28 @@ module Tracco
         end
 
         it "converts the effort in hours when the notification contains an effort in days" do
-          with_message("@trackinguser +1.5d") { |t| t.effort.amount.should == 8+4 }
-          with_message("@trackinguser +1.5g") { |t| t.effort.amount.should == 8+4 }
+          with_message("@test_tracker +1.5d") { |t| t.effort.amount.should == 8+4 }
+          with_message("@test_tracker +1.5g") { |t| t.effort.amount.should == 8+4 }
         end
 
         it "converts the effort in hours when the notification contains an effort in pomodori" do
-          with_message("@trackinguser +10p") { |t| t.effort.amount.should == 5}
+          with_message("@test_tracker +10p") { |t| t.effort.amount.should == 5}
         end
 
         it "fetch the effort from a complex effort message" do
-          with_message "@trackinguser ho speso +2h e spero che stavolta possiamo rilasciarla" do |tracking|
+          with_message "@test_tracker ho speso +2h e spero che stavolta possiamo rilasciarla" do |tracking|
             tracking.effort.amount.should == 2.0
           end
         end
 
         it "fetch the effort even when beween square brackets" do
-          with_message "@trackinguser [+0.5h]" do |tracking|
+          with_message "@test_tracker [+0.5h]" do |tracking|
             tracking.effort.amount.should == 0.5
           end
         end
 
         it "computes the effort considering all the mentioned team mates in the message" do
-          with_message "@trackinguser +2h assieme a @michelepangrazzi e @alessandrodescovi" do |tracking|
+          with_message "@test_tracker +2h assieme a @michelepangrazzi e @alessandrodescovi" do |tracking|
             tracking.effort.amount.should == 2.0 * 3
           end
         end
@@ -64,7 +71,7 @@ module Tracco
             Trello::Member.should_receive(:find).with(username).and_return(self.send(username))
           end
 
-          notification = create_notification(data: { 'text' => "@trackinguser +2h assieme a @michelepangrazzi e @alessandrodescovi" },
+          notification = create_notification(data: { 'text' => "@test_tracker +2h assieme a @michelepangrazzi e @alessandrodescovi" },
                                              member_creator: stub(username: "pietrodibello"))
           with notification do |tracking|
             tracking.effort.members.should == [michelepangrazzi, alessandrodescovi, pietrodibello]
@@ -76,7 +83,7 @@ module Tracco
             Trello::Member.should_receive(:find).with(username).and_return(self.send(username))
           end
 
-          notification = create_notification(data: { 'text' => "@trackinguser +3p (@alessandrodescovi @michelevincenzi)" },
+          notification = create_notification(data: { 'text' => "@test_tracker +3p (@alessandrodescovi @michelevincenzi)" },
                                              member_creator: stub(username: "pietrodibello"))
 
           with notification do |tracking|
@@ -86,7 +93,7 @@ module Tracco
         end
 
         it "tracks the effort with the date given in the notification text, not the actual notification date" do
-          raw_data = create_notification(data: { 'text' => "@trackinguser 22.11.2012 +6p" }, date: "2012-09-19T12:46:13.713Z")
+          raw_data = create_notification(data: { 'text' => "@test_tracker 22.11.2012 +6p" }, date: "2012-09-19T12:46:13.713Z")
 
           tracking = Tracking::Factory.build_from(raw_data)
 
@@ -94,7 +101,7 @@ module Tracco
         end
 
         it "tracks the effort to yesterday when the keyword 'yesterday' is present before the effort amount" do
-          raw_data = create_notification(data: { 'text' => "@trackinguser yesterday +6p" }, date: "2012-09-19T12:46:13.713Z")
+          raw_data = create_notification(data: { 'text' => "@test_tracker yesterday +6p" }, date: "2012-09-19T12:46:13.713Z")
 
           tracking = Tracking::Factory.build_from(raw_data)
 
@@ -102,7 +109,7 @@ module Tracco
         end
 
         it "tracks the effort to yesterday when the keyword 'yesterday' is present before the effort amount" do
-          raw_data = create_notification(data: { 'text' => "@trackinguser +6p yesterday" }, date: "2012-09-19T12:46:13.713Z")
+          raw_data = create_notification(data: { 'text' => "@test_tracker +6p yesterday" }, date: "2012-09-19T12:46:13.713Z")
 
           tracking = Tracking::Factory.build_from(raw_data)
 

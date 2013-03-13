0e6a555a19c87293f03a9da906ced1faed35e8a5
Pietro Di Bello <pietro.dibello@xpeppers.com>
2013-03-14 00:02:41 +0100

    migrating the last rake task to bin executable. Still leaving the rake task, marking them as DEPRECATED though

--- lib/tasks/tasks.rake
+++ lib/tasks/tasks.rake
@@ -2,7 +2,7 @@
 desc "Open an irb session preloaded with this library, e.g. rake 'console[production]' will open a irb session with the production db env"
 task :console, [:tracco_env] do |t, args|
   args.with_defaults(tracco_env: "development")
-  sh "tracco console --environment #{args.tracco_env}"
+  sh "tracco c --environment #{args.tracco_env}"
 end
 
 # DEPRECATED: use tracco executables instead
@@ -11,32 +11,18 @@ task :c, [:tracco_env] do |t, args|
   sh "tracco c --environment #{args.tracco_env}"
 end
 
-desc "Copy template config files"
-task :prepare do
-  Dir.glob("config/*.template.yml").each do |file|
-    template_file = File.basename(file)
-    target_file = template_file.sub('.template', '')
-    if File.exists?(File.join('config', target_file))
-      puts "skipping #{target_file.color(:yellow)}, already exists."
-    else
-      cp File.join('config', template_file), File.join('config', target_file)
-      puts "please edit the #{target_file} to have all the proper configurations"
-    end
-  end
-end
-
 # DEPRECATED: use tracco executables instead
 namespace :run do
   desc "Run on the cards tracked starting from a given day, e.g. rake 'run:from_day[2012-11-1]'"
   task :from_day, [:starting_date, :tracco_env] do |t, args|
     args.with_defaults(starting_date: Date.today.to_s, tracco_env: "development")
-    sh "tracco collect #{args.starting_date} --environment #{args.tracco_env}"
+    sh "tracco t #{args.starting_date} --environment #{args.tracco_env}"
   end
 
   desc "Run on the cards tracked today, #{Date.today}"
   task :today, [:tracco_env] do |t, args|
     args.with_defaults(tracco_env: "development")
-    sh "tracco collect #{Date.today.to_s} --environment #{args.tracco_env}"
+    sh "tracco t #{Date.today.to_s} --environment #{args.tracco_env}"
   end
 end
 
@@ -45,6 +31,6 @@ namespace :export do
   desc "Export all cards to a google docs spreadsheet, e.g. rake \"export:google_docs[my_sheet,tracking,production]\""
   task :google_docs, [:spreadsheet, :worksheet, :tracco_env] do |t, args|
     args.with_defaults(tracco_env: "development")
-    sh "tracco export_google_docs #{args.spreadsheet} #{args.worksheet} --environment #{args.tracco_env}"
+    sh "tracco egd #{args.spreadsheet} #{args.worksheet} --environment #{args.tracco_env}"
   end
 end
--- lib/tracco/cli.rb
+++ lib/tracco/cli.rb
@@ -7,40 +7,72 @@ module Tracco
     include Thor::Actions
     include TrelloConfiguration
 
-    desc "console", "Opens an irb session preloaded with Tracco gem"
+    def self.source_root
+      File.join(File.dirname(__FILE__), "..", "..", "config")
+    end
+
+    map "c" => :console
+    map %w(track t) => :collect
+    map "egd" => :export_google_docs
+    map "check_env" => :ensure_env
+    map %w(-v --version) => :version
+    map %w(initialize --initialize) => :init
+
+    desc "console", "Opens up an irb session preloaded with Tracco gem"
     long_desc <<-LONGDESC
-      Opens an irb session preloaded with this library.
+      Opens up an irb session preloaded with this library.
       e.g. 'tracco console production' will open a irb session with the production environment
     LONGDESC
     method_option :environment, :aliases => "-e", :desc => "the env to use", :default => "development"
     def console
       environment = environment_from(options)
-
       run "export TRACCO_ENV=#{environment}; irb -rubygems -I lib -r tracco -r startup_trello.rb -r awesome_print"
     end
-    map %w(c) => :console
 
 
     desc "collect STARTING_FROM", "Runs tracking data fetching on the cards tracked starting from a given date"
+    long_desc <<-LONGDESC
+      Runs tracking data fetching on the cards tracked starting from a given date.
+      e.g. 'tracco 2013.03.23' will start collecting tracking data starting from March 23, 2013.
+    LONGDESC
     method_option :environment,         :aliases => "-e", :desc => "the env to use", :default => "development"
     method_option :mongoid_config_path, :aliases => "-m", :desc => "the mongoid config file to use"
-    method_option :verbose, :type => :boolean, :aliases => "-v", :default => false
-    def collect(starting_date=Date.today.to_s)
+    method_option :verbose,             :aliases => "-v", :type => :boolean, :default => false
+    def collect(starting_date_as_string=Date.today.to_s)
       set_logger_level(options[:verbose])
       invoke :ensure_env
 
-      starting_date = Date.today.to_s if starting_date == "today"
-      error("Invalid date: #{starting_date}") unless is_valid_date?(starting_date)
-
+      starting_date = date_from(starting_date_as_string)
       environment = environment_from(options)
       Tracco::Database.load_env(environment, options[:mongoid_config_path])
 
       puts "Collecting tracking data starting from #{starting_date} in the #{environment} env."
       tracker = Tracco::TrelloTracker.new
-      tracker.track(Date.parse(starting_date))
+      tracker.track(starting_date)
+    end
+
+    desc "initialize", "Copy template configuration files"
+    def init
+      Dir.glob("config/*.template.yml").each do |file|
+        template_file = File.basename(file)
+        target_file = template_file.sub('.template', '')
+
+        if File.exists?(File.join('config', target_file))
+          say "skipping #{target_file.color(:yellow)}, already exists."
+        else
+          copy_file File.join(CLI.source_root, template_file), File.join(CLI.source_root, target_file)
+          say "please edit the #{target_file} to have all the proper configurations"
+        end
+      end
     end
 
     desc "export_google_docs SPREADSHEET_NAME WORKSHEET_NAME", "Exports tracking data to google docs"
+    long_desc <<-LONGDESC
+      Exports all tracking data stored in the db into a google docs.
+      e.g. 'tracco export_google_docs tracco_export tracking_data' will export tracking data into a 'tracco_export' sheet,
+      in the 'tracking_data' worksheet.
+    LONGDESC
+
     method_option :environment,         :aliases => "-e", :desc => "the env to use", :default => "development"
     method_option :mongoid_config_path, :aliases => "-m", :desc => "the mongoid config file to use"
     method_option :verbose, :type => :boolean, :aliases => "-v", :default => false
@@ -56,16 +88,14 @@ module Tracco
       say "[DONE]".color(:green)
       say "Go to #{spreadsheet_url}"
     end
-    map %w(egd) => :export_google_docs
 
 
     desc "version", "Prints Tracco's version information"
     def version
       say "Tracco version #{Tracco::VERSION}"
     end
-    map %w(-v --version) => :version
 
-    desc "ensure_env", "Ensures the environment is valid and prints the current configuration params"
+    desc "ensure_env", "Ensures the environment is valid"
     def ensure_env(*args)
       %w{developer_public_key access_token_key}.each do |each_name|
         unless ENV[each_name] || authorization_params_from_config_file[each_name]
@@ -76,7 +106,6 @@ module Tracco
         error("Missing <tracker_username> configuration parameter: set it as environment variable or in the config/config.yml file.")
       end
     end
-    map %w(check_env) => :ensure_environment
 
     private
 
@@ -91,12 +120,20 @@ module Tracco
       return environment
     end
 
-    def is_valid_env?(environment)
-      %w{production development test}.include? environment
+    def date_from(starting_date_as_string)
+      if starting_date_as_string == "today"
+        Date.today
+      else
+        parse_date(starting_date_as_string) || error("Invalid date: #{starting_date_as_string}")
+      end
     end
 
-    def is_valid_date?(date)
-      Date.parse(date) rescue nil
+    def parse_date(date_as_string)
+      Date.parse(date_as_string) rescue nil
+    end
+
+    def is_valid_env?(environment)
+      %w{production development test}.include? environment
     end
 
     def error(message)
